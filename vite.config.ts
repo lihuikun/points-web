e6e80218a3827aaf3d60ef96dc67041740379c41
lihuikun <lihuikun@wezonet.com>
2025-01-06 14:04:57 +0800

    fix: 环境变量bug

--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,26 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import Components from 'unplugin-vue-components/vite'
 import { VantResolver } from 'unplugin-vue-components/resolvers'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [
-    vue(),
-    Components({
-      resolvers: [VantResolver()]
-    })
-  ],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://lihk.serv00.net:13381/api',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, '') // 可选：重写路径
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd())
+  console.log(env)
+  return {
+    plugins: [
+      vue(),
+      Components({
+        resolvers: [VantResolver()]
+      })
+    ],
+    server: {
+      proxy: {
+        '/api': {
+          target: env.VITE_API_URL,
+          changeOrigin: true,
+          rewrite: (path) => path.replace(/^\/api/, '')
+        }
       }
     }
   }
